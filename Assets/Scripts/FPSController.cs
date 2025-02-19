fcbd2b00dfb39f8844cb5393f4eda155de148cf7
Caleb Chung <kaichun24@gmail.com>
2025-02-19 00:56:19 -0800

    changed char model and the char touches the groudn

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -6,9 +6,9 @@ using UnityEngine;
 public class FPSController : MonoBehaviour
 {
     public Camera playerCamera;
-    public float walkSpeed = 6f;
-    public float runSpeed = 12f;
-    public float jumpPower = 7f;
+    public float walkSpeed = 3f;
+    public float runSpeed = 6f;
+    public float jumpPower = 6f;
     public float gravity = 14f;
 
 
@@ -42,8 +42,8 @@ public class FPSController : MonoBehaviour
 
         // Press Left Shift to run
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxisRaw("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxisRaw("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
